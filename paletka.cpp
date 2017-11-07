30e3ef8772cea38aec5a889afa6a149fb45ff5ea
ketjowp <32547860+ketjowp@users.noreply.github.com>
2017-11-07 01:52:43 +0100

    Update and rename Paletka.cpp to paletka.cpp
    Rozdzielenie klasy na dwa pliki - źródłowy i nagłówkowy

--- paletka.cpp
+++ paletka.cpp
@@ -1,33 +1,7 @@
-
+#include "paletka.h"
+#include <list>
 #include <Windows.h>
 
-class Paletka
-{
-public:
-	Paletka();
-	~Paletka();
-	void paletkastart(float x, float y, int height, int width, int velocity, HWND hwnd);
-	float GetX()const;
-	float GetY()const;
-	int GetWidth()const;
-	int GetHeight()const;
-	int GetVelocity()const;
-	void SetX(int x);
-	void SetY(int y);
-	void SetWidth(int size);
-	void SetHeight(int size);
-	void SetVelocity(int veloc);
-	void rysujpaletke(HDC hdcMem, HWND hwnd);
-	BOOLEAN NaPaletceX(float x, int size, int velocity);
-	BOOLEAN NaPaletceY(float y, int size, int velocity);
-	void Reset(RECT r);
-
-private:
-	POINT coordinates;
-	int height;
-	int width;
-	int velocity;
-};
 Paletka::Paletka()
 {
 	this->coordinates.x = 0;
