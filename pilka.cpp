13a5ecc1cde2d8c0ff3caea7643b932ef58413b1
ketjowp <32547860+ketjowp@users.noreply.github.com>
2017-11-07 01:56:47 +0100

    Update pilka.cpp

--- pilka.cpp
+++ pilka.cpp
@@ -1,35 +1,5 @@
-
+#include "pilka.h"
 #include <windows.h>
-
-
-class Pilka
-{
-public:
-	Pilka();
-	~Pilka();
-	void pilkastart(int x, int y, int size, int velocityX, int velocityY, HWND hwnd);
-	int GetX()const;
-	int GetY()const;
-	int GetSize()const;
-	int GetVelocityX()const;
-	int GetVelocityY()const;
-	void SetX(int x);
-	void SetY(int y);
-	void SetSize(int size);
-	void SetVelocityX(int veloc);
-	void SetVelocityY(int veloc);
-	void Reset(RECT r);
-	BOOLEAN GetStart()const;
-	void SetStart(BOOLEAN start);
-	void rysujpilke(HDC hdcMem, HWND hwnd);
-
-private:
-	POINT coordinates;
-	int size;
-	float velocityX;
-	float velocityY;
-	BOOLEAN start;
-};
 
 Pilka::Pilka()
 {
